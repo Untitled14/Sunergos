43d8246bad5b66ccc6cb206659ac0a3f72fa6eb4
ricardas-mikelionis <ricardas.mikelionis@gmail.com>
2017-11-25 15:59:48 +0200

    Added tutorial
    First 3 tutorials

---
+++ Assets/TutorialMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessage : MonoBehaviour {
+	private TriggerConnection _trigger;
+	private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		_trigger = GetComponent<TriggerConnection> ();
+		_spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_trigger.Triggered) {
+			//gameObject.SetActive (true);
+			Color newColor = _spriteRenderer.color;
+			newColor.a = 1;
+			_spriteRenderer.color = newColor;
+		} else {
+			//gameObject.SetActive (false);
+			Color newColor = _spriteRenderer.color;
+			newColor.a = 0;
+			_spriteRenderer.color = newColor;
+		}
+	}
+}
